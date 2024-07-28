ee005734dd67ab963441fafd8dadb7aedcd95411
Ramsey <someramseyy@gmail.com>
2024-07-28 15:46:57 +0200

    Updated

--- src/index.js
+++ src/index.js
@@ -1,21 +1,297 @@
-require('dotenv').config();
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, ChannelType, Client, GatewayIntentBits, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+import "dotenv/config";
+import fs from "fs";
+import puppeteer from "puppeteer";
 
-const http = require('http');
-const port = process.env.PORT;
+let updateInterval;
+let runtimeData = loadRuntimeData();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!');
+const ALLOWED_USERS = process.env.ALLOWED_USERS.split(",");
+const SERVER_CONFIGURE_PAGE = process.env.SERVER_CONFIGURE_PAGE + "/" + process.env.SERVER_ID;
+const SERVER_CONFIGURATION_ENDPOINT = process.env.SERVER_CONFIGURATION_ENDPOINT + "/" + process.env.SERVER_ID;
+
+const browser = await puppeteer.launch({ headless: false, userDataDir: process.env.PUPPETEER_USERDATA_PATH });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.once("ready", () => {
+    console.log("Discord client ready");
+
+    if (runtimeData.channelId && runtimeData.messageId) {
+        fetchUpdateMessage().then(updateMessage => {
+            startUpdateInterval();
+            update(updateMessage);
+        }).catch(error => {
+            console.error("Initial fetch for update message failed, error:", error);
+        })
+    } else {
+        console.log("No channel id or message id available, skipping initial update");
+    }
 });
 
-server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+client.on("interactionCreate", async interaction => {
+    if (!ALLOWED_USERS.includes(interaction.user.id)) {
+        await interaction.reply({ content: '👎', ephemeral: true });
+        return;
+    }
+
+    if (interaction.isCommand()) {
+        handleCommand(interaction);
+    } else if (interaction.isModalSubmit()) {
+        handleModal(interaction);
+    } else if (interaction.isButton()) {
+        handleButton(interaction);
+    }
 });
 
-server.on('error', (err) => {
-    console.error('Server error:', err);
-});
 
-server.on('listening', () => {
-    console.log('Server is up and running');
-});
+client.login(process.env.DISCORD_BOT_TOKEN);
+
+async function handleButton(interaction) {
+    switch (interaction.customId) {
+        case 'dismiss-alert': {
+            const channel = await interaction.client.channels.fetch(interaction.channelId);
+            const message = await channel.messages.fetch(interaction.message.id);
+
+            await message.delete();
+        }
+    }
+}
+
+async function handleModal(interaction) {
+    switch (interaction.customId) {
+        case 'update-token-modal': {
+            runtimeData.authToken = interaction.fields.getTextInputValue("tokenInput");
+            saveRuntimeData();
+            console.log("Updated auth token:", runtimeData.authToken);
+
+            await interaction.reply({ content: ':man_playing_water_polo::skin-tone-5: ', ephemeral: true });
+        } break;
+    }
+}
+
+async function handleCommand(interaction) {
+    const { commandName } = interaction;
+
+    switch (commandName) {
+        case "init": {
+            if (interaction?.channel?.type !== ChannelType.GuildText) {
+                await interaction.reply("🙅🏿‍♂️");
+                return;
+            }
+
+            const updateMessage = await interaction.channel.send("...");
+
+            runtimeData.channelId = interaction.channelId;
+            runtimeData.messageId = updateMessage.id;
+
+            saveRuntimeData();
+
+            clearInterval(updateInterval);
+            startUpdateInterval();
+            update(updateMessage);
+
+            await interaction.deferReply();
+            await interaction.deleteReply();
+
+        } break;
+
+        case "update": {
+            const favoriteColorInput = new TextInputBuilder()
+                .setCustomId('tokenInput')
+                .setLabel("Token")
+                .setRequired(true)
+                .setStyle(TextInputStyle.Paragraph);
+
+            const row = new ActionRowBuilder()
+                .addComponents(favoriteColorInput);
+
+            const modal = new ModalBuilder()
+                .setCustomId('update-token-modal')
+                .setTitle('Modal')
+                .addComponents(row)
+
+            await interaction.showModal(modal);
+        } break;
+
+        case "trigger": {
+            clearInterval(updateInterval);
+
+            fetchUpdateMessage().then(async updateMessage => {
+                startUpdateInterval();
+                update(updateMessage);
+
+                await interaction.reply({
+                    content: "👍",
+                    ephemeral: true
+                });
+            }).catch(async error => {
+                console.error("[TRIGGER] Failed to fetch update message, error:", error);
+
+                await interaction.reply({
+                    content: "MESAJ YOK 😱😱 \n" + error,
+                    ephemeral: true
+                });
+            });
+        } break;
+
+        default: {
+            console.error("Unhandled command:", commandName);
+        }
+    }
+
+}
+
+async function alert(message) {
+    ALLOWED_USERS.forEach(async alertId => {
+        try {
+            const user = await client.users.fetch(alertId);
+
+            if (!user) {
+                console.log("failed to fetch user for alert, id: '" + alertId + "'");
+                return;
+            }
+
+            const dismissButton = new ButtonBuilder()
+                .setStyle(ButtonStyle.Secondary)
+                .setLabel("👍")
+                .setCustomId("dismiss-alert");
+
+            const row = new ActionRowBuilder()
+                .addComponents(dismissButton);
+
+            await user.send({
+                content: message,
+                components: [row]
+            });
+        } catch (error) {
+            console.error("Failed to send alert to user, error:", error);
+        }
+    });
+}
+
+let minorAlertLevel = 0;
+
+async function update(updateMessage) {
+    try {
+        if (!runtimeData.authToken) {
+            console.error("No auth token available");
+            alert("TOKEN ❌❌ 😭 👎👎");
+            clearInterval(updateInterval);
+            return;
+        }
+
+        const page = await browser.newPage();
+
+        await page.goto('https://roblox.com');
+        await page.setCookie({ "name": ".ROBLOSECURITY", "value": runtimeData.authToken, "domain": ".roblox.com", "session": false });
+
+        await page.goto(SERVER_CONFIGURE_PAGE);
+
+        if (page.url() !== SERVER_CONFIGURE_PAGE) {
+            console.error("Failed to login using token");
+            alert("TOKEN BOZUK 👎👎😭 🗣️🔥🔥");
+            clearInterval(updateInterval);
+            return;
+        }
+
+        const generateButton = await page.waitForSelector("#generate-link-button");
+
+        if (!generateButton) {
+            console.error("Selector for generate button failed, possible corrupted state");
+
+            if (minorAlertLevel >= 1) {
+                alert("Ramseyi çağır 🙏🙏 💀❓ 👎👎😭 🗣️🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥");
+                clearInterval(updateInterval);
+                minorAlertLevel = 0;
+                return;
+            }
+
+            minorAlertLevel++;
+            return;
+        }
+
+        page.on("response", async response => {
+            if (response.url() == SERVER_CONFIGURATION_ENDPOINT) {
+                if (response.request().method() !== "PATCH") {
+                    return;
+                }
+
+                if (response.status() !== 200) {
+                    console.error("Failed to update server configuration, generate request failed status code:", response.status());
+
+                    if (minorAlertLevel >= 3) {
+                        alert("Ramseyi çağır 🙏🙏 😱👆👇☝️👈👉 👎👎👎");
+                        clearInterval(updateInterval);
+                        minorAlertLevel = 0;
+                        return;
+                    }
+
+                    minorAlertLevel++;
+                    return;
+                }
+
+
+                const body = await response.json();
+
+                if (!body.link) {
+                    console.log("Failed to update server configuration, invalid body:", body);
+
+                    if (minorAlertLevel >= 3) {
+                        alert("İşi batırdım, ramseyi çağır 🙏🙏 😱🙅‍♂️");
+                        clearInterval(updateInterval);
+                        minorAlertLevel = 0;
+                    }
+
+                    minorAlertLevel++;
+                }
+
+
+                minorAlertLevel = 0;
+                updateMessage.edit(body.link);
+
+                console.log("Updated server link:", body.link);
+                page.close();
+            }
+        })
+
+        await generateButton.click();
+    } catch (error) {
+        console.error("Failed to update server link unhandled error:", error);
+        alert("RAMSEYI ÇAĞIIIIR 🙏🙏🙏🙏🙏🙏 🚨🚨🚨\n||"+error+"||");
+        clearInterval(updateInterval);
+    }
+}
+
+function startUpdateInterval() {
+    updateInterval = setInterval(() => {
+        fetchUpdateMessage().then(updateMessage => {
+            update(updateMessage);
+        }).catch(error => {
+            console.error("[Interval] Failed to fetch update message, error:", error);
+            clearInterval(updateInterval);
+            alert("Mesaj gitti 😱\n||`"+ error + '||');
+        });
+    }, process.env.DISCORD_BOT_UPDATE_INTERVAL);
+}
+
+function fetchUpdateMessage() {
+    return new Promise((resolve, reject) => {
+        client.channels.fetch(runtimeData.channelId).then(channel => {
+            channel.messages.fetch(runtimeData.messageId).then(message => {
+                resolve(message);
+            }).catch(reject);
+        }).catch(reject);
+    });
+}
+
+function saveRuntimeData() {
+    fs.writeFileSync(process.env.RUNTIME_DATA_PATH, JSON.stringify(runtimeData));
+}
+
+function loadRuntimeData() {
+    if (!fs.existsSync(process.env.RUNTIME_DATA_PATH)) {
+        return {};
+    }
+
+    return JSON.parse(fs.readFileSync(process.env.RUNTIME_DATA_PATH));
+}
---
+++ src/register.js
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import { REST, Routes } from 'discord.js';
+
+const rest = new REST();
+rest.setToken(process.env.DISCORD_BOT_TOKEN);
+
+const route = Routes.applicationCommands(process.env.DISCORD_CLIENT_ID);
+
+const commands = [
+    {
+        name: 'init',
+        description: 'init'
+    },
+    {
+        name: 'update',
+        description: 'update'
+    },
+    {
+        name: 'trigger',
+        description: 'trigger'
+    }
+];
+rest.put(route, { body: commands })
+    .then(() => console.log("Successfully registered application commands"))
+    .catch(console.error);
